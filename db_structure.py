cf8687aa0bb696045c66a7fe874eecd3b56c6a9c
Benkralex <ben_github@enkrich.de>
2024-09-22 18:07:34 +0200

    clean up 3 + user-administration

--- db_structure.py
+++
@@ -1,4 +0,0 @@
-from main import db, app
-
-
-
--- routes.py
+++ routes.py
@@ -37,7 +37,8 @@ def index_page():
     if "username" not in session:
         return login_page()
     subjects, topics = get_topics_and_subjects()
-    return render_template("index.html", subjects=subjects, topics=topics, user=session["username"])
+    active = {"index": "active"}
+    return render_template("index.html", subjects=subjects, topics=topics, active=active, user=session["username"], admin=session["admin"])
 
 @app.route('/<subject>/<topic>', strict_slashes=False)
 def topic_page(subject, topic):
@@ -47,7 +48,8 @@ def topic_page(subject, topic):
     subject = Subjects.query.filter_by(name=subject).first()
     topic = Topics.query.filter_by(title=topic, subject_id=subject.id).first()
     subjects, topics = get_topics_and_subjects()
-    return render_template("topic.html", akt_topic=topic, akt_subject=subject, subjects=subjects, topics=topics)
+    active = {subject.name: "active"}
+    return render_template("topic.html", akt_topic=topic, akt_subject=subject, active=active, subjects=subjects, topics=topics, admin=session["admin"])
 
 @app.route('/<subject>/<topic>/question', strict_slashes=False, methods=['GET', 'POST'])
 def question_page(subject, topic):
@@ -62,7 +64,7 @@ def question_page(subject, topic):
         db.session.commit()
         return redirect(url_for("topic_page", subject=subject, topic=topic.title))
     else:
-        return render_template("question.html", subject=subject, topic=topic)
+        return redirect(url_for("topic_page", subject=subject, topic=topic))
 
 @app.route('/search', strict_slashes=False, methods=['GET', 'POST'])
 def search_page():
@@ -72,15 +74,21 @@ def search_page():
         query = escape(request.args.get("query"))
         res_topics = Topics.query.filter(Topics.title.contains(query)).all()
         res_topics.extend(Topics.query.filter(Topics.content.contains(query)).all())
-        subjects = Subjects.query.all()
+        res_topics = list(set(res_topics)) #Delete duplicates
+
+        subjects, topics = get_topics_and_subjects()
+        active = {}
         subject_map = {}
         for subject in subjects:
             subject_map[subject.id] = subject.name
-        subjects, topics = get_topics_and_subjects()
-        return render_template("search.html", results=res_topics, subject_map=subject_map, subjects=subjects, topics=topics, query=query)
+
+        return render_template("search.html", results=res_topics, subject_map=subject_map, active=active, subjects=subjects, topics=topics, query=query, admin=session["admin"])
     else:
         return redirect(url_for("index_page"))
 
+#####################
+# Account
+#####################
 @app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
 def login_page():
     if request.method == "POST":
@@ -92,14 +100,16 @@ def login_page():
                 session["name"] = user.name
                 session["username"] = user.username
                 session["role"] = user.role
+                session["admin"] = user.role in ["admin", "owner"]
+                session["id"] = user.id
                 redirect_to = session.pop("redirect_to", url_for("index_page"))
                 return redirect(redirect_to)
             else:
-                return render_template("login.html", error="password")
+                return render_template("account/login.html", error="password")
         else:
-            return render_template("login.html", error="username")
+            return render_template("account/login.html", error="username")
     else:
-        return render_template("login.html", error="")
+        return render_template("account/login.html", error="")
 
 @app.route('/register', methods=['GET', 'POST'], strict_slashes=False)
 def register_page():
@@ -111,15 +121,15 @@ def register_page():
         hashed_password = hash_password(password)
         role = escape("default")
         if not re.match(username_pattern, username):
-            return render_template("register.html", error="username", name=name, email=email, username=username)
+            return render_template("account/register.html", error="username", name=name, email=email, username=username)
         if not re.match(password_pattern, password):
-            return render_template("register.html", error="password", name=name, email=email, username=username)
+            return render_template("account/register.html", error="password", name=name, email=email, username=username)
         if not re.match(email_pattern, email):
-            return render_template("register.html", error="email", name=name, email=email, username=username)
+            return render_template("account/register.html", error="email", name=name, email=email, username=username)
         if not re.match(name_pattern, name):
-            return render_template("register.html", error="name", name=name, email=email, username=username)
+            return render_template("account/register.html", error="name", name=name, email=email, username=username)
         if Users.query.filter_by(username=username).first():
-            return render_template("register.html", error="username", name=name, email=email, username=username)
+            return render_template("account/register.html", error="username", name=name, email=email, username=username)
         user = Users(name=name, username=username, email=email, password=hashed_password, role=role)
         db.session.add(user)
         db.session.commit()
@@ -128,10 +138,12 @@ def register_page():
             session["name"] = user.name
             session["username"] = user.username
             session["role"] = user.role
+            session["admin"] = user.role in ["admin", "owner"]
+            session["id"] = user.id
             return redirect(url_for("index_page"))
         return redirect(url_for("login_page"))
     else:
-        return render_template("register.html", error="", name="", email="", username="")
+        return render_template("account/register.html", error="", name="", email="", username="")
 
 @app.route('/logout', strict_slashes=False)
 def logout_page():
@@ -139,3 +151,72 @@ def logout_page():
     session.pop("username", None)
     session.pop("role", None)
     return redirect(url_for("index_page"))
+
+#####################
+# Administration
+#####################
+@app.route('/administration', strict_slashes=False)
+def administration_page():
+    if "username" not in session or not session["admin"]:
+        return redirect(url_for("index_page"))
+    subjects, topics = get_topics_and_subjects()
+    active = {"admin": "active"}
+    return render_template("administration/index.html", subjects=subjects, topics=topics, user=session["username"], admin=session["admin"], active=active)
+
+@app.route('/administration/subjects', strict_slashes=False)
+def administration_subject_page():
+    if "username" not in session or not session["admin"]:
+        return redirect(url_for("index_page"))
+    subjects, topics = get_topics_and_subjects()
+    active = {"admin": "active"}
+    return render_template("administration/subjects.html", subjects=subjects, topics=topics, user=session["username"], admin=session["admin"], active=active)
+
+@app.route('/administration/users', strict_slashes=False)
+def administration_user_page():
+    if "username" not in session or not session["admin"]:
+        return redirect(url_for("index_page"))
+    users = Users.query.all()
+    subjects, topics = get_topics_and_subjects()
+    active = {"admin": "active"}
+    return render_template("administration/users.html", subjects=subjects, topics=topics, users=users, user=session, admin=session["admin"], active=active, role=session["role"])
+
+@app.route('/administration/users/delete/<user_id>', strict_slashes=False)
+def administration_user_delete_page(user_id):
+    if "username" not in session or not session["admin"]:
+        return redirect(url_for("index_page"))
+    if not Users.query.filter_by(id=user_id).first():
+        return redirect(url_for("administration_user_page"))
+    if Users.query.filter_by(id=user_id).first().id == session["id"]:
+        return redirect(url_for("administration_user_page"))
+    if Users.query.filter_by(id=user_id).first().role in ["admin", "owner"]:
+        if session["role"] != "owner":
+            return redirect(url_for("administration_user_page"))
+    Users.query.filter_by(id=user_id).delete()
+    db.session.commit()
+    return redirect(url_for("administration_user_page"))
+
+@app.route('/administration/users/edit/<user_id>', strict_slashes=False, methods=['GET', 'POST'])
+def administration_user_edit_page(user_id):
+    if "username" not in session or not session["admin"]:
+        return redirect(url_for("index_page"))
+    user = Users.query.filter_by(id=user_id).first()
+    if not user:
+        return redirect(url_for("administration_user_page"))
+    if request.method == "POST":
+        role = escape(request.form["role"])
+        if not role in ["default", "admin", "owner"]:
+            return redirect(url_for("administration_user_page"))
+        user.role = role
+        db.session.commit()
+        return redirect(url_for("administration_user_page"))
+    else:
+        return redirect(url_for("administration_user_page"))
+
+@app.route('/administration/topics', strict_slashes=False)
+def administration_topic_page():
+    if "username" not in session or not session["admin"]:
+        return redirect(url_for("index_page"))
+    users = Users.query.all()
+    subjects, topics = get_topics_and_subjects()
+    active = {"admin": "active"}
+    return render_template("administration/topics.html", subjects=subjects, topics=topics, users=users, user=session["username"], admin=session["admin"], active=active)
